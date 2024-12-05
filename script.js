5cbdb6fffa0efb0f3352ab8cc1d9e76a39f60c9a
Jose Fernando <caldeirajfr@gmail.com>
2024-12-05 16:07:05 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,16 @@
+operacoesBasicas(4, 5);
+
+function operacoesBasicas(num1, num2) {
+  console.log(`${num1} + ${num2} = ${num1 + num2}`);
+  console.log(`${num1} - ${num2} = ${num1 - num2}`);
+  console.log(`${num1} x ${num2} = ${num1 * num2}`);
+  console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
+
+const num3 = 4
+const num4 = 5
+
+console.log(`A soma de ${num3} e ${num4} resulta ${num3 + num4}`);
+console.log(`A subtração de ${num3} por ${num4} resulta ${num3 - num4}`);
+console.log(`A multiplicação de ${num3} por ${num4} resulta ${num3 * num4}`);
+console.log(`A divisão de ${num3} por ${num4} resulta ${num3 / num4}`);
